3a28452e75c8baaa1ef54ab213d76eb3fa7108fb
Arman <solaa8239@gmail.com>
2025-07-28 09:27:58 +0330

    not found route added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Login } from "./components/Login";
 import { Register } from "./components/Register";
+import { NotFound } from "./components/NotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route index element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
